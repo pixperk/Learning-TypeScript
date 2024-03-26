7683b695294d46fe9f3917c6937ae9ab6a3f07e3
Yashaswi Kumar Mishra <mishrayashaswikumar@gmail.com>
2024-03-26 15:47:33 +0530

    redux tk with ts

--- rtk/src/App.tsx
+++ rtk/src/App.tsx
@@ -1,6 +1,29 @@
+import { useState } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { StateType, incrementByValue, increment, decrement } from "./redux"
+
 const App = () => {
+
+  const [val, setVal] = useState<number>(0)
+
+const dispatch = useDispatch()
+
+const count = useSelector((state:StateType)=>state.count)
+
+  const incrementByValueHandler = ()=>{
+    dispatch(incrementByValue(val))
+    setVal(0)
+  }
+
   return (
-    <div>App</div>
+    <div>
+      <h1>RTK with TS</h1>
+      <h2>Count:{count}</h2>
+      <button onClick={()=>dispatch(increment())}>+</button>
+      <button onClick={()=>dispatch(decrement())}>-</button>
+      <input type="number" value={val} onChange={(e)=>{setVal(Number(e.target.value))}} />
+      <button disabled={val<0} onClick={incrementByValueHandler}>Add</button>
+    </div>
   )
 }
 
--- rtk/src/redux.ts
+++ rtk/src/redux.ts
@@ -24,26 +24,30 @@ export const store = configureStore({reducer:rootReducer}) */
 
 //USING createslice
 
-import { configureStore, createAction, createReducer  } from "@reduxjs/toolkit";
+import { PayloadAction, configureStore, createSlice } from "@reduxjs/toolkit";
 
-interface StateType{
-    count:number
+export interface StateType {
+  count: number;
 }
 
-const increment=createAction("increment")
-const decrement=createAction("decrement")
+const initialState: StateType = { count: 0 };
 
-const initialState:StateType = {
-    count:0
-}
+const rootSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.count += 1;
+    },
+    decrement: (state) => {
+      state.count -= 1;
+    },
+    incrementByValue:(state,action:PayloadAction<number>)=>{
+        state.count+=action.payload
+    }
+  },
+});
 
-const rootReducer = createReducer(initialState,(builder)=>{
-builder.addCase(increment,(state)=>{
-    state.count+=1
-})
-.addCase(decrement,(state)=>{
-    state.count-=1
-})
-})
+export const {increment,decrement, incrementByValue} = rootSlice.actions
 
-export const store = configureStore({reducer:rootReducer})
+export const store = configureStore({ reducer: rootSlice.reducer });
